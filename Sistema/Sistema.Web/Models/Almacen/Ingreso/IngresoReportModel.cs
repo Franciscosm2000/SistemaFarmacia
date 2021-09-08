4a048509acb04df468bca1f7458a2e849ddb319d
franciscosm2000 <franciscosandoval2000@hotmail.com>
2021-09-08 14:10:35 -0600

    Agregando solo dos roles y metodos de reportes

---
+++ Sistema/Sistema.Web/Models/Almacen/Ingreso/IngresoReportModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Web.Models.Almacen.Ingreso
+{
+    public class IngresoReportModel
+    {
+        public string codigo_arti { get; set; }
+        public string nom_arti { get; set; }
+        public int cantidad { get; set; }
+        public decimal valor { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
---
+++ Sistema/Sistema.Web/Models/Ventas/Venta/VentaReportModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Web.Models.Ventas.Venta
+{
+    public class VentaReportModel
+    {
+        public string codigo_arti { get; set; }
+        public string nom_arti { get; set; }
+        public int cantidad { get; set; }
+        public decimal valor { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
